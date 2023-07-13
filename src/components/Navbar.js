9ef2d767caefc32bf236ca08249ce5e071d29be5
Joyce Lai <joycelylai@gmail.com>
2023-07-13 12:32:28 -0400

    signout attempt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,6 +29,12 @@ export default function Navbar() {
             Log In
           </Link>
           <Link
+            href="/auth/signout"
+            className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+          >
+            Log Out
+          </Link>
+          <Link
             href="/game"
             className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
           >
