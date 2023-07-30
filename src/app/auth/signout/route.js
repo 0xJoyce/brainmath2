47e2ec3fb7d29971bd5db2ef973c75a8b538dc53
Mathieu Lue <mathieu.lue@gmail.com>
2023-07-30 15:48:49 -0400

    force dynamic on routes

--- src/app/auth/signout/route.js
+++ src/app/auth/signout/route.js
@@ -7,6 +7,8 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 ///This GET async function was changed from POST per Keenan.  Link components do GET requests by default.  This solves the Nav bar inconsistency issue.
+export const dynamic = "force-dynamic"; //clodflare solution
+
 export async function GET(req) {
   const supabase = createRouteHandlerClient({ cookies });
   console.log("Accessing /signout/route.js ");
