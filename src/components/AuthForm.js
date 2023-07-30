8d12d60ef8a8e14ace2c856be911c3824ae2361c
Mathieu Lue <mathieu.lue@gmail.com>
2023-07-30 16:11:01 -0400

    next url

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -16,7 +16,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={["google"]}
-      redirectTo="https://brainmath2-pe2a0itm5-0xjoyce.vercel.app/auth/callback"
+      redirectTo={`${process.env.VERCEL_URL}/auth/callback`}
     />
   );
 }
